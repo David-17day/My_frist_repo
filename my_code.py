40e97e37c95467154ab6eaf76a5a23024dec67e8
Luis David <daviluis1709@gmail.com>
2025-03-21 22:34:16 -0500

    Calculo_de_areas_versio_1

---
+++ my_code.py
@@ -0,0 +1,33 @@
+import math
+def area_cuadrado():
+    lado = float(input("Ingresa el lado del cuadrado: "))
+    return lado**2
+def area_triangulo():
+    base = float(input("Ingrese la base del triangulo: "))
+    altura= float(input("Ingrese la altura del triangulo: "))
+    return (base*altura)/2
+def area_circulo():
+    radio = float(input("Ingrese el radio del circulo: "))
+    return math.pi * (radio**2)
+
+while True:
+    print("-------MENU DE CÁLCULO DE ÁREAS------")
+    print("1. Calculo de area un cuadrado")
+    print("2. Calculo de area un triangulo")
+    print("3. Calculo de area circulo")
+    print("4. Salir del menu")
+    
+    opcion = input("Seleccione una opcion: ")
+    
+    if opcion == "1":
+        print(f"El area del cuadrado es : {area_cuadrado()}")
+    elif opcion == "2":
+        print(f"El area del triangulo es: {area_triangulo()}")
+    elif opcion == "3":
+        print(f"El area del circulo es: {area_circulo()}")
+    elif opcion == "4":
+        print("Ha salido del menu")
+        break
+    else:
+        print('Opcion no valida - Error')
+        
